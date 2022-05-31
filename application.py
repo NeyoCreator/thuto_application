a268784c0495a9e90745eca109a6ad4398714ea8
Neo Sebanze <neo@bidvestalice.com>
2022-05-31 08:32:10 +0200

    CLEANED THE CODE

--- application.py
+++ application.py
@@ -3,7 +3,8 @@ from tkinter import *
 from tkinter import ttk
 from PIL import Image, ImageTk
 from tkinter import filedialog as fd
-from tkinter.messagebox import showinfo
+# from tkinter.messagebox import showinfo
+import os
 
 #1.FRAMES
 win = Tk()
@@ -21,42 +22,22 @@ def select_file():
         initialdir='/',
         filetypes=filetypes)
 
-    showinfo(
-        title='Selected File',
-        message=filename
-    )
+    # showinfo(
+    #     title='Selected File',
+    #     message=filename
+    # )
     x =filename[-10:]
 
+    head, tail = os.path.split(filename)
+    print(tail)
+
     # IMAGE STUFF
-    image1 = Image.open(f"images/{x}")
+    image1 = Image.open(f"images/{tail}")
     test = ImageTk.PhotoImage(image1)
     label1 = ttk.Label(image=test)
     label1.image = test
     label1.place(x=50, y=50)
-    label1.forget()
-    return x
 
-#2.SHOW FUNCTION
-def show_widget():
-   label.pack()
-   label1.pack()
-   b1.configure(text="Hide", command=hide_widget)
-
-#3.HIDE FUNCTION
-def hide_widget():
-   label.pack_forget()
-   label1.forget()
-   b1.configure(text="Show", command=show_widget)
-
-
-
-#4.DISPLAY BUTTON
-b1 = ttk.Button(win, text="Hide", command=hide_widget)
-b1.pack(pady=20)
-
-# Add a label widget
-label = ttk.Label(win, text="Eat, Sleep, Code and Repeat", font=('Aerial 11'))
-label.pack(pady=30)
 
 
 
