4188c3c5fabbfca2227d34f7b7c55373b1cf4246
Neo Sebanze <neo@bidvestalice.com>
2022-05-31 08:22:56 +0200

    MANGED TO LOAD IMAGE WITH A BUTTON

--- application.py
+++ application.py
@@ -1,27 +1,56 @@
-# Import the required libraries
+#0.IMPORTS
 from tkinter import *
 from tkinter import ttk
 from PIL import Image, ImageTk
+from tkinter import filedialog as fd
+from tkinter.messagebox import showinfo
 
-# Create an instance of tkinter frame
+#1.FRAMES
 win = Tk()
-
-# Set the size of the tkinter window
 win.geometry("700x350")
 
+def select_file():
+    global label1
+    filetypes = (
+        ('PNG image', '*.png'),
+        ('All files', '*.*')
+    )
 
-# Define a function to show/hide widget
+    filename = fd.askopenfilename(
+        title='Open a file',
+        initialdir='/',
+        filetypes=filetypes)
+
+    showinfo(
+        title='Selected File',
+        message=filename
+    )
+    x =filename[-10:]
+
+    # IMAGE STUFF
+    image1 = Image.open(f"images/{x}")
+    test = ImageTk.PhotoImage(image1)
+    label1 = ttk.Label(image=test)
+    label1.image = test
+    label1.place(x=50, y=50)
+    label1.forget()
+    return x
+
+#2.SHOW FUNCTION
 def show_widget():
    label.pack()
    label1.pack()
    b1.configure(text="Hide", command=hide_widget)
 
+#3.HIDE FUNCTION
 def hide_widget():
    label.pack_forget()
    label1.forget()
    b1.configure(text="Show", command=show_widget)
 
-# Add a Button widget
+
+
+#4.DISPLAY BUTTON
 b1 = ttk.Button(win, text="Hide", command=hide_widget)
 b1.pack(pady=20)
 
@@ -30,12 +59,14 @@ label = ttk.Label(win, text="Eat, Sleep, Code and Repeat", font=('Aerial 11'))
 label.pack(pady=30)
 
 
-# IMAGE STUFF
-image1 = Image.open("image1.PNG")
-test = ImageTk.PhotoImage(image1)
-label1 = ttk.Label(image=test)
-label1.image = test
-# label1.place(x=50, y=50)
-# label1.forget()
+
+# open button
+open_button = ttk.Button(
+    win,
+    text='Open a File',
+    command=select_file
+)
+
+open_button.pack(expand=True)
 
 win.mainloop()
